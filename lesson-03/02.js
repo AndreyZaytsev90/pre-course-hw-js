5f963df5f1f30ff9fbd5b0df1d4b0716386de935
zaytsev <zaicev@mniti.ru>
2024-03-21 10:00:50 +0300

    Lesson 3, exercise 2 - completed

---
+++ lesson-03/02.js
@@ -0,0 +1,5 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+const searchOrange = words.indexOf("апельсин")
+searchOrange >= 0 ? console.log("Ура! нашел") : console.log("Придется поискать в другом магазине…")
+
