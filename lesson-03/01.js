c94b9e7c2391a6c23e547da13a25fb812649fd2f
zaytsev <zaicev@mniti.ru>
2024-03-21 09:36:14 +0300

    Lesson 3, exercise 1 - completed

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((el) => {
+        return el === 'черный' || el === 'желтый' || el === 'красный'
+    }).join('-')
+}
+
+console.log(createColorString());
+
